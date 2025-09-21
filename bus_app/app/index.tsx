1f614928c114ed55ca9408a660e12354f4beee36
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-09-21 23:52:20 +0530

    5th commit

--- bus_app/app/index.tsx
+++ bus_app/app/index.tsx
@@ -1,4 +1,6 @@
 import { Text, View } from "react-native";
+import { Link } from "expo-router";
+
 
 export default function Index() {
   return (
@@ -10,6 +12,11 @@ export default function Index() {
       }}
     >
       <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Link href="/login" style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}>Login Page</Link>
     </View>
   );
 }
