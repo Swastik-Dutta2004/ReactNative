98c6706a6896b9d35085523b14d414b2309a03f1
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-09-22 00:44:17 +0530

    2nd commit

--- bus_app/app/(tabs)/_layout.tsx
+++ bus_app/app/(tabs)/_layout.tsx
@@ -1,10 +1,17 @@
 import { Tabs } from "expo-router";
+import Feather from '@expo/vector-icons/Feather';
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
 
 export default function TabsLayout() {
   return (
-    <Tabs screenOptions={{tabBarActiveTintColor:"coral"}}>
-      <Tabs.Screen name="index" options={{title: "Home"}}/>
-      <Tabs.Screen name="login" options={{title: "Login"}}/>
+    <Tabs screenOptions={{ tabBarActiveTintColor: "coral" }}>
+      <Tabs.Screen name="index" options={{
+        title: "Home", tabBarIcon: ({ color, focused }) => {
+          return focused ? <FontAwesome5 name="home" size={24} color={color} /> :
+            <Feather name="home" size={24} color={color} />
+        },
+      }} />
+      <Tabs.Screen name="login" options={{ title: "Login" }} />
     </Tabs>
   )
 }
