f96f424f7ab78f5cdcf840bff2b1d4ccb56740a6
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-09-22 13:13:44 +0530

    8th commit

--- bus_app/app/auth.tsx
+++ bus_app/app/auth.tsx
@@ -1,16 +1,49 @@
-import { KeyboardAvoidingView, Platform, View} from "react-native";
-import {Text, TextInput} from 'react-native-paper'
+import { KeyboardAvoidingView, Platform, View, StyleSheet } from "react-native";
+import { Button, Text, TextInput, } from 'react-native-paper'
+import { useState } from "react";
 
-export default function AuthScreen(){
-    return <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"}>
-        <View>
-            <Text>Create Account</Text>
-            <TextInput
-            label="Email"
-            autoCapitalize="none"
-            keyboardType="email-address"
-            placeholder="example@gmail.com"
-            />
-        </View>
-    </KeyboardAvoidingView>
+export default function AuthScreen() {
+    const [isSignUp, setisSignUp] = useState<boolean>(false)
+
+    const HandleSwitch = () => {
+        setisSignUp((prev) => !prev)
+    }
+
+    return (
+        <KeyboardAvoidingView
+         behavior={Platform.OS === "ios" ? "padding" : "height"}
+         style = {styles.container}
+         >
+            <View>
+                <Text>{isSignUp ? "Create Account" : "Welcome Back"}</Text>
+
+                <TextInput
+                    label="Email"
+                    autoCapitalize="none"
+                    keyboardType="email-address"
+                    placeholder="example@gmail.com"
+                    mode="outlined"
+                />
+
+                <TextInput
+                    label="Password"
+                    autoCapitalize="none"
+                    keyboardType="email-address"
+                    placeholder="example@gmail.com"
+                    mode="outlined"
+                />
+
+                <Button mode="contained">{isSignUp ? "Sign Up" : "Sign In"}</Button>
+
+                <Button mode="text" onPress={HandleSwitch}>{isSignUp ? "Already have an account? Sign In" : "Don't have an account? Sign up"}</Button>
+            </View>
+        </KeyboardAvoidingView>
+    )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#f5f5f5"
+    }
+})
