a0c59ffd362d188db1ff1cb1d5ce8f9d7a07d118
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-09-22 21:47:47 +0530

    11th commit

--- bus_app/lib/auth_context.tsx
+++ bus_app/lib/auth_context.tsx
@@ -1,16 +1,36 @@
 import React, { createContext } from "react";
-import { Models } from "react-native-appwrite";
+import { ID, Models } from "react-native-appwrite";
+import { account } from "./appwrite";
 
 type AuthContextType = {
     user: Models.User<Models.Preferences> | null
+    signUp: (email: string, Password: string) => Promise<void>
+    signIn: (email: string, Password: string) => Promise<void>
 }
 
 const AuthContext = createContext(undefined)
 
-export function AuthProvider ({children} : {children: React.ReactNode}){
-    return <></>
+export function AuthProvider({ children }: { children: React.ReactNode }) {
+    const signUp = async(email: string, Password: string) => {
+        try{
+            await account.create(ID.unique(), email, Password)
+        }catch(error){
+
+        }
+    }
+    const signIn = async(email: string, Password: string) => {
+        try{
+            await account.create(ID.unique(), email, Password)
+        }catch(error){
+
+        }
+    }
+    return (
+        <AuthContext.Provider value={{user, signUp, signIn}}>
+            {children}
+        </AuthContext.Provider>)
 }
 
-export function useAuth(){
+export function useAuth() {
 
 }
